45a774901bd0857e1438dc7fb8ff854db71116f8
VaidyaHarshal <harshal.vaidya300@gmail.com>
2021-11-24 18:25:48 -0500

    added functions to display, modify and update reviews. Changes in the seed file and package json file

--- data/parkingReviews.js
+++ data/parkingReviews.js
@@ -1,13 +1,16 @@
 const mongoCollections = require('../config/mongoCollections');
-const parkings = mongoCollections.parkings
-const parkingReviews = mongoCollections.parkingReviews
-const {ObjectId} = require('mongodb');
+const parkings = mongoCollections.parkings;
+// const parkingReviews = mongoCollections.parkingReviews
+const { ObjectId } = require('mongodb');
 
 let exportedMethods = {
-    async createReview(parkingId, userId, rating, comment) {
+    async createReview(parkingId, userId, rating, dateOfReview, comment) {
+        let arrayOfObject = [];
+        let avgRating = 0;
         const parkingCollection = await parkings();
         const checkParking = await parkingCollection.findOne({ _id: ObjectId(parkingId)});
         
+        console.log(ObjectId(parkingId));
         if(checkParking === null)
             throw 'Parking does not exist';
 
@@ -16,6 +19,7 @@ let exportedMethods = {
             parkingId: parkingId,
             userId: userId,
             rating: rating,
+            dateOfReview: dateOfReview,
             comment: comment
         }
 
@@ -33,14 +37,15 @@ let exportedMethods = {
             throw 'Parking does not exist, review cannot be displayed';
 
         arrayOfObject.push(sameReview);
+        console.log(arrayOfObject);
 
         arrayOfObject.forEach(element => {
-            element.reviews.forEach(data => {
+            element.parkingReviews.forEach(data => {
                 avgRating += data.rating;
             });
         });
 
-        avgRating = (avgRating/sameReview.reviews.length);
+        avgRating = (avgRating/sameReview.parkingReviews.length);
 
         const reviewUpdate = await parkingCollection.updateOne(
             {_id: ObjectId(parkingId)},
@@ -49,7 +54,7 @@ let exportedMethods = {
 
         if(!reviewUpdate.matchedCount && !reviewUpdate.modifiedCount)
             throw 'Updating overallRating cannot be done';
-
+        
         const parkingSpace = await parkingCollection.findOne({ _id: ObjectId(parkingId)});
 
         if(parkingSpace === null)
@@ -58,7 +63,7 @@ let exportedMethods = {
         parkingSpace._id = parkingSpace._id.toString();
 
         for(let key in parkingSpace) {
-            if(typeof parkingSpace[key] === 'object' && key === "reviews") {
+            if(typeof parkingSpace[key] === 'object' && key === "parkingReviews") {
                 if(Array.isArray(parkingSpace[key])) {
                     for(let i = 0; i < parkingSpace[key].length; i++) {
                         parkingSpace[key][i]._id = parkingSpace[key][i]._id.toString();
@@ -71,5 +76,135 @@ let exportedMethods = {
 
     async getAllReviews(parkingId) {
         const parkingCollection = new parkings()
+        const parking = await parkingCollection.findOne({_id: ObjectId(parkingId)})
+
+        if(parking === null)
+            throw 'No parking found with that ID';
+
+        parking.parkingReviews.forEach(element => {
+            element._id = element._id.toString();
+        })
+        return parking.parkingReviews;
+    },
+
+    async getReview(reviewId) {
+        let resultData = {};
+        const parkingCollection = await parkings()
+        const parking = await parkingCollection.find({}).toArray();
+
+        if(parking === null)
+            throw 'No review present with that ID';
+
+        parking.forEach(element => {
+            element.parkingReviews.forEach(data => {
+                if(data._id.toString() === reviewId) {
+                    resultData = {"_id": data._id, "parkingId": data.parkingId, "userId": data.userId, "rating": data.rating, "dateOfReview": data.dateOfReview, "comment": data.comment};
+                }
+            })
+        });
+    
+        resultData._id = resultData._id.toString();
+        return resultData;
+    },
+
+    async removeReview(reviewId) {
+        let parkingId = "";
+        let avgRating = 0;
+        let resultData = {};        
+        console.log("Inside the remove review by ID function");
+        const parkingCollection = await parkings();
+        const parking = await parkingCollection.find({}).toArray();
+    
+        if(parking === null)
+            throw 'No review present with that Id';
+
+        parking.forEach(element => {
+            element.parkingReviews.forEach(data => {
+                if(data._id.toString() === reviewId) {
+                    parkingId = element._id;
+                }
+            })
+        });
+
+        const removeReview = await parkingCollection.updateOne({}, {$pull: {parkingReviews: {_id: ObjectId(reviewId)}}});
+    
+        if(!removeReview.matchedCount && !removeReview.modifiedCount)
+            throw 'Removal of review has failed';
+        
+        const parkReview = await parkingCollection.find({}).toArray();
+    
+        if(parkReview === null)
+            throw 'No review present with that Id';
+
+        parkReview.forEach(element => {
+            if(element._id.toString() === parkingId.toString()) {
+                element.parkingReviews.forEach(data => {
+                    avgRating += data.rating;
+                })
+                avgRating = (avgRating/element.parkingReviews.length);
+            }
+        });
+    
+        const reviewUpdate = await parkingCollection.updateOne(
+            {_id: ObjectId(parkingId)},
+            {$set: {overallRating: avgRating}}
+        )
+    
+        if(!reviewUpdate.matchedCount && !reviewUpdate.modifiedCount)
+            throw 'Update of the rating has been failed';
+
+        resultData = {"reviewId": reviewId, "deleted": true};
+        console.log(resultData);    
+        return resultData;
+    },
+
+    async updateReview(reviewId, rating, comment) {
+        const parkingCollection = await parkings();
+        const findReview = await parkingCollection.findOne({_id: ObjectId(reviewId)})
+
+        if(findReview === null)
+            throw 'Review does not exist';
+
+        let reviewUpdateInfo = {
+            rating: rating,
+            comment: comment
+        };
+
+        const updateReview = await parkingCollection.updateOne(
+            {_id: ObjectId(reviewId)},
+            {$set: {parkingReviews: reviewUpdateInfo}}
+        );
+
+        if(!updateReview.matchedCount && !updateReview.modifiedCount)
+            throw 'Update has been failed';
+
+        if(!updateReview.modifiedCount)
+            throw 'Same values has been provided for update. Please change the values';
+
+        const parkReview = await parkingCollection.find({}).toArray();
+    
+        if(parkReview === null)
+            throw 'No review present with that Id';
+
+        parkReview.forEach(element => {
+            if(element._id.toString() === parkReview.parkingId.toString()) {
+                element.parkingReviews.forEach(data => {
+                    avgRating += data.rating;
+                })
+                avgRating = (avgRating/element.parkingReviews.length);
+            }
+        });
+    
+        const ratingUpdate = await parkingCollection.updateOne(
+            {_id: ObjectId(parkReview.parkingId)},
+            {$set: {overallRating: avgRating}}
+        )
+    
+        if(!ratingUpdate.matchedCount && !ratingUpdate.modifiedCount)
+            throw 'Update of the rating has been failed';
+
+        return await this.getReview(reviewId);
     }
 }
+
+module.exports = exportedMethods;
--- tasks/seed.js
+++ tasks/seed.js
@@ -1,10 +1,13 @@
 const { json } = require("express");
 const connection = require("../config/mongoConnection");
+const parkingReviews  = require("../data/parkingReviews");
 const parkings = require("../data/parkings");
 
 const main = async () => {
   try {
     try {
+      const db = await connection();
+      await db.dropDatabase();
       firstRestaurant = await parkings.createParkings(
         "6164f085181bfcb0325557c6",
         [],
@@ -19,13 +22,25 @@ const main = async () => {
         []
       );
       console.log(firstRestaurant);
-      const db = await connection();
-      await db._connection.close();
+      //  await db.serverConfig.close();
+
+      //    const db = await connection();
+      console.log(firstRestaurant._id);
+      firstReview = await parkingReviews.createReview(
+        firstRestaurant._id,
+        "7174f085181bfcb0325557c8",
+        5,
+        "11/24/2021",
+        "This is a very nice space"
+      );
+
+    console.log(firstReview);
+    await db.connection.close();
     } catch (error) {
       console.log(error);
     }
   } catch (error) {}
-};
+}  
 
 main().catch((error) => {
   console.log(error);
