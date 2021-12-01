3b50fbc9566abeb7938021b9f118b5ba057c6264
shreyasvispute <vispute.shreyas1@gmail.com>
2021-12-01 15:39:39 -0500

    rename index to app changed package.json

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const static = express.static(__dirname + "/public");
+var bodyParser = require("body-parser");
+const path = require("path");
+
+const configRoutes = require("./routes");
+const exphbs = require("express-handlebars");
+
+app.use("/public", static);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+configRoutes(app);
+
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log("Your routes will be running on http://localhost:3000");
+});
