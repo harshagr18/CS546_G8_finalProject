a4246ae8676670fee8561c0bedc4788732e222a6
shreyasvispute <vispute.shreyas1@gmail.com>
2021-12-02 02:02:16 -0500

    changed js script to dashboard js file added form validation for dashboard

---
+++ public/js/dashboard.js
@@ -0,0 +1,72 @@
+(function ($) {
+  //error functions
+
+  $("#uiError").hide();
+  //edit parkings
+  // $("#editParkings").click(function (e) {
+  //   e.preventDefault();
+  //   let parkingId = $("#parkingID").val();
+  //   let requestConfig = {
+  //     method: "PUT",
+  //     url: window.location.href + "/" + parkingId,
+  //   };
+  //   $.ajax(requestConfig).then(function (response) {
+  //     console.log(response);
+  //   });
+  // });
+
+  //   $("#createParkings").click(function (e) {
+  //     e.preventDefault();
+  //     let requestConfig = {
+  //       method: "GET",
+  //       url: window.location.href + "/create",
+  //     };
+  //     $.ajax(requestConfig).then(function (response) {});
+  //   });
+
+  //   $("#btnMainSearch").click(function (e) {
+  //     error.hide();
+  //     e.preventDefault();
+  //     let mainSearch = $("#mainSearch").val();
+  //     if (mainSearch.trim().length === 0) {
+  //       error.show();
+  //       error.html("Search box cannot be empty");
+  //     }
+  //     let result = checkAlphanumerics(mainSearch);
+  //     if (result) {
+  //       error.show();
+  //       error.html("Search only accepts alphanumeric");
+  //     }
+  //     let searchParams = new URLSearchParams(window.location.search);
+  //     console.log(searchParams.has("mainSearch"));
+
+  //     let requestConfig = {
+  //       method: "GET",
+  //       url: location.href,
+  //     };
+  //   });
+  //
+})(jQuery);
+
+function checkAlphanumerics(phrase) {
+  let str = phrase;
+  const checker = /[^a-z0-9]/g;
+  if (checker.test(str)) {
+    return true;
+  }
+
+  return false;
+}
+//form validation before submit
+function searchValidation(event) {
+  let citysearch = $("#citySearch").val();
+  let statesearch = $("#stateSearch").val();
+  let zipsearch = $("#zipSearch").val();
+  if (!citysearch && !statesearch && !zipsearch) {
+    event.preventDefault();
+    $("#uiError").show();
+    $("#uiError").html("Search atleast by city, zipcode or state");
+    return false;
+  }
+  return true;
+}
