8fca479a6b647ef78314f724fea12e9b8331d8f5
shreyasvispute <vispute.shreyas1@gmail.com>
2021-12-03 02:30:23 -0500

    images, index- / route first

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const parkingRoutes = require("./parkings");
 const userRoutes = require("./users");
 const dashboardRoutes = require("./dashboard");
-const reviewRoutes = require('./parkingReviews');
+const reviewRoutes = require("./parkingReviews");
 const constructorMethod = (app) => {
+  //keep "/" route first always!
+  app.use("/", dashboardRoutes);
   app.use("/users", userRoutes);
   app.use("/parkings", parkingRoutes);
-  app.use ('/reviews', reviewRoutes);
-  app.use("/", dashboardRoutes);
+  app.use("/reviews", reviewRoutes);
   app.use("*", (req, res) => {
     res.status(404).json({ error: "Path Not Found" });
   });
