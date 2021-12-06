ecd6243938e01c65914f198340249d121bdddb38
shreyasvispute <vispute.shreyas1@gmail.com>
2021-12-06 01:21:17 -0500

    added axios for distance matrix api added html validators changes

--- routes/parkings.js
+++ routes/parkings.js
@@ -32,6 +32,22 @@ const upload = multer({
   storage: storage,
 });
 
+//geocoding api
+// const axios = require("axios");
+// const params = {
+//   access_key: "YOUR_ACCESS_KEY",
+//   query: "1600 Pennsylvania Ave NW",
+// };
+
+// axios
+//   .get("https://api.positionstack.com/v1/forward", { params })
+//   .then((response) => {
+//     console.log(response.data);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
 //get the lister id
 router.get("/", async (req, res) => {
   try {
