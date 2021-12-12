633e92e1738fe7be29e8822a563fbbd75f570712
harshagr18 <harshagrawal1802@gmail.com>
2021-12-12 18:58:50 -0400

    Converting login to AJAX

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,24 @@
+const { parseTwoDigitYear } = require("moment");
+const { use } = require("../../routes/users");
+
 (function ($) {})(jQuery);
 
-function updateUserValidation(event) {
+function createUserValidation(event) {
+  event.preventDefault();
   let username = $("#username").val();
   let password = $("#password").val();
+
+  var requestConfig = {
+    method: "POST",
+    url: "/users/login",
+    data: { username: username, password: password },
+  };
+
+  $.ajax(requestConfig).then(function (responseMessage) {
+    var newElement = $(responseMessage);
+    window.location.replace("/");
+  });
+
   if (username.trim().length == 0) {
     $("#uiError").show();
     $("#uiError").html("Please Enter Username");
