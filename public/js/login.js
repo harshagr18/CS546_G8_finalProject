2fed9c89018faad6ed3b3ccb550b865558d758cc
shreyasvispute <vispute.shreyas1@gmail.com>
2021-12-12 22:01:56 -0500

    login ajax changes

--- public/js/login.js
+++ public/js/login.js
@@ -1,32 +1,36 @@
-const { parseTwoDigitYear } = require("moment");
-const { use } = require("../../routes/users");
+(function ($) {
+  let errormsg = $("#login-error");
+  errormsg.hide();
 
-(function ($) {})(jQuery);
+  $("#loginbtn").click(function (event) {
+    event.preventDefault();
+    let username = $("#username").val();
+    let password = $("#password").val();
+    if (username.trim().length == 0) {
+      $("#uiError").show();
+      $("#uiError").html("Please Enter Username");
+      return false;
+    } else if (password.trim().length == 0) {
+      $("#uiError").show();
+      $("#uiError").html("Please Enter Password");
+      return false;
+    }
+    debugger;
+    var requestConfig = {
+      method: "POST",
+      url: location.href,
+      data: { username: username, password: password },
+    };
 
-function createUserValidation(event) {
-  event.preventDefault();
-  let username = $("#username").val();
-  let password = $("#password").val();
-
-  var requestConfig = {
-    method: "POST",
-    url: "/users/login",
-    data: { username: username, password: password },
-  };
-
-  $.ajax(requestConfig).then(function (responseMessage) {
-    var newElement = $(responseMessage);
-    window.location.replace("/");
+    $.ajax(requestConfig)
+      .then(function (res) {
+        debugger;
+        console.log(res);
+        window.location.replace("/");
+      })
+      .fail(function (error) {
+        errormsg.show();
+        errormsg.html(error.responseJSON.error);
+      });
   });
-
-  if (username.trim().length == 0) {
-    $("#uiError").show();
-    $("#uiError").html("Please Enter Username");
-    return false;
-  } else if (password.trim().length == 0) {
-    $("#uiError").show();
-    $("#uiError").html("Please Enter Password");
-    return false;
-  }
-  return true;
-}
+})(jQuery);
